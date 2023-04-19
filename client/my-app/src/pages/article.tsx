fe5f6428d6d8085177b840ca078096b2e8200a4d
Lucas Ramos <lucasramoslobo83@gmail.com>
2023-04-19 16:21:32 -0300

    adiciona respostas aos comentários

--- client/my-app/src/pages/article.tsx
+++ client/my-app/src/pages/article.tsx
@@ -10,25 +10,29 @@ interface ArticleProps {
   comments: {
     likes: number; id: number; id_notice: string; email: string; content: string 
 }[];
-}
+  responses:{
+     id: number; post: number; email: string; content: string 
+}[];
+};
 
-const Article: NextPage<ArticleProps> = ({ id, title, content, comments }) => {
+const Article: NextPage<ArticleProps> = ({ id, title, content, comments, responses }) => {
   const [email, setEmail] = useState("");
   const [comment, setComment] = useState("");
   const [localComments, setLocalComments] = useState(comments);
   const [selectedCommentId, setSelectedCommentId] = useState<number | null>(
     null
   );
+  const [commentResponses, setCommentResponses] = useState(responses);
 
   useEffect(() => {
     const fetchComment = async () => {
       if (selectedCommentId !== null) {
-        const response = await fetch(`http://localhost:8000/api/posts/${selectedCommentId}/`);
-        if (response.ok) {
-          const updatedComment = await response.json();
+        const commentResponse = await fetch(`http://localhost:8000/api/posts/${selectedCommentId}/`);
+        if (commentResponse.ok) {
+          const commentData = await commentResponse.json();
           const updatedComments = localComments.map(comment => {
             if (comment.id === selectedCommentId) {
-              return updatedComment;
+              return commentData;
             }
             return comment;
           });
@@ -38,7 +42,6 @@ const Article: NextPage<ArticleProps> = ({ id, title, content, comments }) => {
         }
       }
     };
-  
     fetchComment();
   }, [selectedCommentId]);
 
@@ -95,38 +98,80 @@ const Article: NextPage<ArticleProps> = ({ id, title, content, comments }) => {
     }
   };
 
+  const handleNewResponse = async (event: React.FormEvent<HTMLFormElement>, id: number) => {
+    event.preventDefault();
+  
+    const formData = new FormData(event.target as HTMLFormElement);
+    const email = formData.get("email") as string;
+    const content = formData.get("content") as string;
+  
+    const data = {
+      id: commentResponses.length + 1,
+      email,
+      content,
+      post: id,
+    };
+  
+    const response = await fetch(`http://localhost:8000/api/posts/responses/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  
+    if (response.ok) {
+      setSelectedCommentId(null);
+  
+      const updatedResponsesResponse = await fetch(`http://localhost:8000/api/posts/responses/`);
+      const updatedResponses = await updatedResponsesResponse.json();
+      setCommentResponses(updatedResponses);
+    }
+  };
+  
   const filteredComments = localComments.filter(
     (comment) => comment.id_notice === id.toString()
   );
-  
-  const commentElements = filteredComments.map((comment) => (
-    <div key={comment.id}>
-      <p>{comment.email}</p>
-      <p>{comment.content}</p>
-      <div className={styles.button_container}>
-        <button className={styles.button} onClick={() => handleLikeClick(comment.id)}>
-          {comment.likes} Curtir
-        </button>
-    <button className={styles.button} onClick={() => handleCommentClick(comment.id)}>
-          Responder
-        </button>
-      </div>
-      {selectedCommentId === comment.id && (
-        <form>
-          <div>
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" />
-          </div>
-          <div>
-            <label htmlFor="comment">Comentário:</label>
-            <textarea id="comment" name="comment" />
-          </div>
-          <button className={styles.button}>Enviar</button>
-        </form>
-      )}
-    </div>
-    ));
 
+  const commentElements = filteredComments.map((comment) => {
+    const responses = commentResponses.filter((r) => r.post === comment.id); // renomeie o parâmetro e use a variável `responses` em vez de `commentResponses`
+    return (
+      <div key={comment.id}>
+        <p>{comment.email}</p>
+        <p>{comment.content}</p>
+        <div className={styles.button_container}>
+          <button className={styles.button} onClick={() => handleLikeClick(comment.id)}>
+            {comment.likes} Curtir
+          </button>
+          <button className={styles.button} onClick={() => handleCommentClick(comment.id)}>
+            Responder
+          </button>
+        </div>
+        {selectedCommentId === comment.id && (
+          <form onSubmit={(event) => handleNewResponse(event, comment.id)}>
+            <div>
+              <label htmlFor="email">Email:</label>
+              <input type="email" id="email" name="email" />
+            </div>
+            <div>
+              <label htmlFor="content">Resposta:</label>
+              <textarea id="content" name="content" />
+            </div>
+            <button className={styles.button}>Enviar</button>
+          </form>
+        )}
+        {responses.length > 0 && responses.map((response) => (
+          <div key={response.id}>
+            <p>Respostas</p>
+            <p>{response.email}</p>
+            <p>{response.content}</p>
+            <div className={styles.button_container}>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  });
   return (
     <>
     <Header />
@@ -171,6 +216,8 @@ export const getServerSideProps: GetServerSideProps<ArticleProps> = async ({
   const article = await res.json();
   const res2 = await fetch(`http://server:8000/api/posts`);
   const comments = await res2.json();
+  const res3 = await fetch(`http://server:8000/api/posts/responses/`);
+  const responses= await res3.json();
 
   return {
     props: {
@@ -178,6 +225,7 @@ export const getServerSideProps: GetServerSideProps<ArticleProps> = async ({
       title: article.title,
       content: article.content,
       comments: comments,
+      responses: responses,
     },
   };
 };
